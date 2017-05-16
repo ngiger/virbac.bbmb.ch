7687c18785de0d04e27933af048dcdf124289330
Niklaus Giger <ngiger@ywesee.com>
2017-05-16 16:01:21 +0200

    Moved start_service to from VIRBAC::App -> BBMB::Util::App

--- lib/bbmb/virbac/app.rb
+++ lib/bbmb/virbac/app.rb
@@ -5,7 +5,7 @@ require 'bbmb/html/util/session'
 require 'sbsm/session'
 require 'sbsm/app'
 require 'ydim/invoice'
-require 'bbmb/util/server'
+require 'bbmb/util/app'
 
 module VIRBAC
   VERSION = `git rev-parse HEAD`
@@ -20,48 +20,6 @@ module VIRBAC
   end
 
   class App < BBMB::Util::App
-    attr_accessor :db_manager, :yus_server
-    attr_accessor :auth, :config, :persistence, :server
-
-    def self.get_server
-      case BBMB.config.persistence
-      when 'odba'
-        DRb.install_id_conv ODBA::DRbIdConv.new
-        @persistence = BBMB::Persistence::ODBA
-      end
-      @auth = DRb::DRbObject.new(nil, BBMB.config.auth_url)
-      @server = BBMB::Util::Server.new(@persistence, self)
-      @server.extend(DRbUndumped)
-      @server
-    end
-
-    def start_service
-      case BBMB.config.persistence
-      when 'odba'
-        DRb.install_id_conv ODBA::DRbIdConv.new
-        @persistence = BBMB::Persistence::ODBA
-      end
-      @auth = DRb::DRbObject.new(nil, BBMB.config.auth_url)
-      puts "installed @auth #{@auth}"
-      @server = App.get_server
-      if(BBMB.config.update?)
-        @server.run_updater
-      end
-      if(BBMB.config.invoice?)
-        @server.run_invoicer
-      end
-      url = BBMB.config.server_url
-      url.untaint
-      DRb.start_service(url, @server)
-      $SAFE = 1
-      $0 = BBMB.config.name
-      SBSM.logger.info('start') { sprintf("starting bbmb-server on %s", url) }
-      DRb.thread.join
-      SBSM.logger.info('finished') { sprintf("starting bbmb-server on %s", url) }
-    rescue Exception => error
-      SBSM.logger.error('fatal') { error }
-      raise
-    end
     def login(email, pass)
       session = BBMB.auth.login(email, pass, BBMB.config.auth_domain)
       Html::Util::KnownUser.new(session)
@@ -75,11 +33,7 @@ module VIRBAC
     def initialize
       SBSM.logger= ChronoLogger.new(BBMB.config.log_pattern)
       SBSM.logger.level = :debug
-      puts "Starting Rack-Service #{self.class} and service #{BBMB.config.server_url}"
       super
-      Thread.new {
-          start_service
-      }
     end
   end
 end
